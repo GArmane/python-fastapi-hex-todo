4b0cf514a4acf2647bcb9061e68b5cd261d3ab84
Giovanni Armane <giovanniarmane@gmail.com>
2020-03-03 01:53:09 -0300

    refactor(todolist/interfaces): change name of application
    Change name of application from server to fastapi

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,5 +1,5 @@
-from todolist.interfaces.server.fastapi import app as web_server
+from todolist.interfaces.fastapi import app as web_app
 
 
 if __name__ == "__main__":
-    print(f"Todolist available interfaces: {[web_server.title]}")
+    print(f"Todolist available interfaces: {[web_app.title]}")
--- todolist/interfaces/fastapi/__init__.py
+++ todolist/interfaces/fastapi/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI  # type: ignore
+from fastapi import FastAPI
 
 
 app = FastAPI()
