0f7dec74253bd5a8e1d9b2a6f7b6e7ee1e2be35c
Giovanni Armane <giovanniarmane@gmail.com>
2020-03-04 01:52:44 -0300

    refactor(todolist): application init and config
    Refactor application init and config

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,8 +1,13 @@
-from todolist.config.inject import configure_inject
+import uvicorn
+from todolist.config.environment import get_initial_settings
 from todolist.interfaces.fastapi import app as web_app
-
-configure_inject()
 
 
 if __name__ == "__main__":
-    print(f"Todolist available interfaces: {[web_app.title]}")
+    conf = get_initial_settings()
+    uvicorn.run(
+        web_app,
+        host=conf.WEB_SERVER_HOST,
+        port=conf.WEB_SERVER_PORT,
+        log_level=conf.LOG_LEVEL,
+    )
--- todolist/config/environment.py
+++ todolist/config/environment.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from dotenv import load_dotenv
 from pydantic import BaseSettings
 
@@ -5,10 +7,27 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     ENV: str
     PYTHONPATH: str
+    LOG_LEVEL: str
     WEB_SERVER_DEBUG: bool
-    WEB_SERVER_TITLE: str
     WEB_SERVER_DESCRIPTION: str
+    WEB_SERVER_HOST: str
+    WEB_SERVER_PORT: int
+    WEB_SERVER_TITLE: str
     WEB_SERVER_VERSION: str
 
 
-load_dotenv()
+def _initial_settings_clojure() -> Callable[[], Settings]:
+    load_dotenv()
+    settings = Settings()
+
+    def func() -> Settings:
+        return settings
+
+    return func
+
+
+get_initial_settings = _initial_settings_clojure()
+
+
+def get_current_settings() -> Settings:
+    return Settings()
