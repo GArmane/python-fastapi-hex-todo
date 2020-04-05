3db85883fe3a16dd91657691e22c786650645d81
Giovanni Armane <giovanniarmane@gmail.com>
2020-04-05 20:27:34 -0300

    chore(todolist.infra.database.seeds): define seeds for todo_item table

--- todolist/infra/database/seeds.py
+++ todolist/infra/database/seeds.py
@@ -1,14 +1,34 @@
 import logging
+from typing import Any, Dict, Iterable
 
 from databases import Database
+from sqlalchemy.schema import Table
+
+from todolist.infra.database.models.todo_item import TodoItem
 from todolist.infra.database.sqlalchemy import database_context, init_database
 
 logger = logging.getLogger(__name__)
 
 
+async def _populate_table(
+    db: Database, table: Table, values: Iterable[Dict[str, Any]],
+):
+    name: str = table.name
+    query = table.insert()
+
+    logger.info(f"Seeding table {name}")
+    await db.execute_many(query, list(values))
+    logger.info(f"Seeded table {name} successfully")
+
+
 async def _populate_todo_item(db: Database) -> None:
-    logger.info("Seeding entity TodoItem")
-    pass
+    values = [
+        {"id": 1, "msg": "Program new awesome web app", "is_done": True},
+        {"id": 2, "msg": "Play videogames", "is_done": True},
+        {"id": 3, "msg": "Wash dishes", "is_done": False},
+        {"id": 4, "msg": "Write blog post", "is_done": False},
+    ]
+    await _populate_table(db, TodoItem, values)
 
 
 async def run() -> None:
